7f0f63874cd79529fe2a072d9acb7a6a76bcc2f7
HmerMza <hkooze7@gmail.com>
2023-11-02 03:47:35 -0500

    creando endpoint de register

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import mysql from "mysql2";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 const port = 2659;
@@ -26,16 +26,17 @@ db.connect((err) => {
 });
 
 app.get("/api", (req, res) => {
-  res.send("esta ingresando a la base ")
+  res.send("esta ingresando a la base ");
 });
 
 // Endpoint para el inicio de sesión
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
-  if (!username || !password) {
+  if (username === undefined || password === undefined) {
+    console.log("aqui entra");
     return res.send({
-      mensaje: "debe completar los campos"
-    })
+      mensaje: "debe completar los campos",
+    });
   }
   db.query(
     "SELECT * FROM users WHERE username = ? AND password = ?",
@@ -59,16 +60,79 @@ app.post("/api/login", (req, res) => {
               res.json({ token });
             }
           );
-
-
         } else {
           res.status(401).send({
-            mensaje: "usuario o contraseña invalidos"
+            mensaje: "usuario o contraseña invalidos",
           });
         }
       }
     }
   );
+});
+
+// Endpoint para el registro de usuarios
+app.post("/api/register", (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ mensaje: "Debe completar todos los campos" });
+  }
+
+  db.query(
+    "INSERT INTO users (username, password) VALUES ( ?, ?)",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ mensaje: "Error al registrar usuario" });
+      } else {
+        res.status(201).json({ mensaje: "Usuario registrado correctamente" });
+      }
+    }
+  );
+});
+
+// Endpoint para obtener todos los usuarios
+app.get("/api/users", (req, res) => {
+  db.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      res.status(500).json({ mensaje: "Error al obtener usuarios" });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+// Endpoint para obtener un usuario por ID
+app.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+
+  db.query("SELECT * FROM users WHERE id = ?", userId, (err, result) => {
+    if (err) {
+      res.status(500).json({ mensaje: "Error al obtener el usuario" });
+    } else {
+      if (result.length > 0) {
+        res.status(200).json(result[0]); // Devuelve el usuario encontrado
+      } else {
+        res.status(404).json({ mensaje: "Usuario no encontrado" });
+      }
+    }
+  });
+});
+
+// Eliminar usuario por ID
+app.delete("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+
+  db.query("DELETE FROM users WHERE id = ?", userId, (err, result) => {
+    if (err) {
+      res.status(500).json({ mensaje: "Error al eliminar el usuario" });
+    } else {
+      if (result.affectedRows > 0) {
+        res.status(200).json({ mensaje: "Usuario eliminado correctamente" });
+      } else {
+        res.status(404).json({ mensaje: "Usuario no encontrado" });
+      }
+    }
+  });
 });
 
 // Ruta protegida
